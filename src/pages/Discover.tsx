1a6bb099bc1f8a0ce26143fc7078169b7e1363f2
Jerry Awoyele <awoyelejerry.14@gmail.com>
2025-03-26 13:54:22 +0100

    Last change on input box

--- src/pages/Discover.tsx
+++ src/pages/Discover.tsx
@@ -343,7 +343,7 @@ export default function Discover() {
           </div>
         </div>
       ) : (
-        <div className="container mx-4 my-8">
+        <div className="container">
           
           <div className="flex flex-col gap-8">
             <div className="flex-1">
